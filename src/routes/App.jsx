0e1a7385e5f3dbab51bbdf8df9c8c935feedad5e
dacier97 <danielor6@hotmail.com>
2023-04-04 21:18:28 -0500

    deploy_1

---
+++ src/routes/App.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Layout from "../containers/Layout";
+import Login from "../containers/Login";
+import RecoveryPassword from "../containers/RecoveryPassword";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+import "../styles/global.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path="/recovery-password"
+          element={
+            <Layout>
+              <RecoveryPassword />
+            </Layout>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Layout>
+              <NotFound />
+            </Layout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
